843979c7148454a79d53a40d492f612d42ebb1c0
Vinicius Linhares Martins <vinicius166@gmail.com>
2023-05-11 11:25:14 -0300

    Layout Edit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
     return (
         <div className="Layout">
             <Outlet />
-            <About />
+            {/* <About /> */}
             <ProjectShoppingList />
         </div>
     )
